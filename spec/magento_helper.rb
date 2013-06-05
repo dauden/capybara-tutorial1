91318da1a3d8de2fc54f1a0d881e32529fbe3f8f
unknown <toanvoa@HCM-SD-0986-WK.HarveyNash.vn.local>
2013-06-05 13:33:24 +0700

    first commit

---
+++ spec/magento_helper.rb
@@ -0,0 +1,22 @@
+module MagentoHelper
+  
+  def go_to_category name
+
+  end
+
+  def navigate_product name
+
+  end
+
+  def check_cart info
+
+  end
+
+  def set_quantity qty
+
+  end
+
+  def remove_item
+
+  end
+end
---
+++ spec/magento_spec.rb
@@ -0,0 +1,37 @@
+
+require 'spec_helper.rb'
+
+feature "Category" do
+
+  # background do
+  #   visit '/'
+  # end
+
+ scenario "Add a simple product to cart" do
+   
+ end
+
+  scenario "Add a configured product to cart" do
+    
+  end
+end
+
+
+feature "Shopping cart" do
+
+  # background do
+  #   visit '/'
+  # end
+  
+  scenario "Update cart" do
+    
+  end
+
+  scenario "Remove item" do
+    
+  end
+
+  scenario "Clear cart" do
+    
+  end
+end
